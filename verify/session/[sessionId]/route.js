f4f7d377b85cedc9262c12e1c1cd9ee673ef070c
Onekyc-dev <onekyc2026@gmail.com>
2026-09-05 16:55:05 +0100

    Delete verify directory

--- verify/session/[sessionId]/route.js
+++
@@ -1,9 +0,0 @@
-import { getVerificationSession } from "../../../../../lib/db";
-
-export async function GET(request, { params }) {
-  const session = await getVerificationSession(params.sessionId);
-  if (!session) {
-    return Response.json({ error: "not_found" }, { status: 404 });
-  }
-  return Response.json(session);
-}
